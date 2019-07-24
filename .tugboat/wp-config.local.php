60f041aaf3269c0f76f5afbb7bca08d411c64d75
tellthemachines <isabel@tellthemachines.com>
2019-07-24 12:40:07 +1000

    Adds base WP config

---
+++ .tugboat/wp-config.local.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Tugboat specific wp-config overrides.
+ *
+ * @package WordPress
+ */
+
+define( 'DB_NAME', 'tugboat' );
+define( 'DB_USER', 'tugboat' );
+define( 'DB_PASSWORD', 'tugboat' );
+define( 'DB_HOST', 'mysql' );
